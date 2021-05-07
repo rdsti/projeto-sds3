9c381bde600893ac48ec5a66d16c96b5ab8acd8d
Rodrigo Donizete Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-05-07 08:16:03 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from 'components/DataTable';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
