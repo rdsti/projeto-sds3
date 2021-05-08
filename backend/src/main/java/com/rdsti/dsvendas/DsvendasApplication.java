299edf86257e36927a8053bac6600b6342d4ebf8
Rodrigo Donizete Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-05-08 20:27:27 -0300

    Security config

--- backend/src/main/java/com/rdsti/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/rdsti/dsvendas/DsvendasApplication.java
@@ -3,6 +3,14 @@ package com.rdsti.dsvendas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @course 	 DEVSUPERIOR - SDS3
+ * @author   Rodrigo Donizete Siqueira
+ * @system   DSVendas
+ * @create   8 de mai. de 2021
+ * @version  1.0
+ */
+
 @SpringBootApplication
 public class DsvendasApplication {
 
