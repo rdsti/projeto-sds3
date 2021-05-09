b4643d8c3565dfe8c2e9ce8e432fbf0e2b642a43
Rodrigo Donizete Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-05-08 21:01:57 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/rdsti/dsvendas/entites/Seller.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package com.rdsti.dsvendas.entites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @course 	 DEVSUPERIOR - SDS3
+ * @author   Rodrigo Donizete Siqueira
+ * @system   DSVendas
+ * @create   8 de mai. de 2021
+ * @version  1.0
+ */
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {
+		
+	}
+
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the sales
+	 */
+	public List<Sale> getSales() {
+		return sales;
+	}
+	
+	
+	
+
+}
